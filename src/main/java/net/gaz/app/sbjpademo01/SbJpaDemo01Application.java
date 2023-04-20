66702398e32650770e47488ab34882771c7e776b
gaz-oracle <fco.gaz.crespo@gmail.com>
2023-04-20 11:33:15 -0600

    Â1º commitÂ

---
+++ src/main/java/net/gaz/app/sbjpademo01/SbJpaDemo01Application.java
@@ -0,0 +1,13 @@
+package net.gaz.app.sbjpademo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJpaDemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJpaDemo01Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/gaz/app/sbjpademo01/SbJpaDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.gaz.app.sbjpademo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbJpaDemo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
