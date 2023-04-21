cd63a42878c2b7a5c3edccfb3b88fcfea976306b
gaz-oracle <fco.gaz.crespo@gmail.com>
2023-04-21 10:54:00 -0600

    1º ex:GenerationType.AUTO

--- src/main/java/net/gaz/app/sbjpademo01/entity/Product.java
+++ src/main/java/net/gaz/app/sbjpademo01/entity/Product.java
@@ -26,7 +26,7 @@ import java.time.LocalDateTime;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Long id;
 
