331dcefc2f65011a91e5c72fa1ca25e891d8e1cb
gaz-oracle <fco.gaz.crespo@gmail.com>
2023-04-27 12:40:11 -0600

    Test for Update

--- src/test/java/net/gaz/app/sbjpademo01/repository/ProductRepositoryTest.java
+++ src/test/java/net/gaz/app/sbjpademo01/repository/ProductRepositoryTest.java
@@ -3,19 +3,21 @@ package net.gaz.app.sbjpademo01.repository;
 import net.gaz.app.sbjpademo01.entity.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
 class ProductRepositoryTest {
 
     @Autowired
     private ProductRepository productRepository;
-
+    /**
+     * 1º Se requiere crear un objeto de Product
+     * 2º Se guardara ese objeto Product
+     * 3º Mostrar la informaccion del producto.
+     */
     @Test
     void saveMethod(){
         //1º
@@ -33,13 +35,23 @@ class ProductRepositoryTest {
         //3º
         System.out.println(saveObjetc.getId());
         System.out.println(saveObjetc.toString());
+    }
 
+    @Test
+    void updateUsingMethod(){
+        //1º Find or retrive an entity by id from database.
+        Long id = 1L;
+        Product product = productRepository.findById(id).get();
+        /**llame al método Get para obtener un objeto de producto de la clase opcional*/
+
+        //2º Actualizar: recuperar la entidad y actualizar la informaccion:
+        product.setName("Update product 1 - 12:33");
+        product.setDescription("Producto Actualizado recientemente");
+
+        //3º Guardar la entidad actualizada en la tabla de las base de Datos.
+        productRepository.save(product);
+        /** EntityManager merge() method */
 
     }
 
 }
-/**
- * 1º Se requiere crear un objeto de Product
- * 2º Se guardara ese objeto Product
- * 3º Mostrar la informaccion del producto.
- */
